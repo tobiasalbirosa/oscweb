664bcb1e3689d305746952f049e97ea7292c353b
tobiasalbirosa <tobiasalbirosa@gmail.com>
2021-09-01 15:40:27 -0300

    m

--- server.js
+++ server.js
@@ -41,7 +41,7 @@ var getIPAddresses = function () {
 console.log("HOST",HOST)
 var udpPort = new osc.UDPPort({
     localAddress: "oscweb.herokuapp.com",
-    localPort: 5000
+    localPort: 5001
 })
 udpPort.on("ready", function () {
     io.sockets.setMaxListeners(1)
