aa81e9167492c136fcf9521377eda2a4b50b8f78
tobiasalbirosa <tobiasalbirosa@gmail.com>
2021-09-02 00:19:44 -0300

    m

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ var osc = require("osc")
 const express = require('express')
 const controller = require('./controller/controller')
 var HOST = 'oscweb.herokuapp.com'
-var PORT = 443 
+var PORT = 3000 
 //APP
 const app = express()
 app.engine('html', require('ejs').renderFile)
