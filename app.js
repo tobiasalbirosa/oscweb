738299b958ec41dd08361e96eb4e5f380996565f
tobiasalbirosa <tobiasalbirosa@gmail.com>
2021-09-01 22:27:33 -0300

    m

--- app.js
+++ app.js
@@ -10,42 +10,25 @@ app.set('view engine', 'html')
 const HOST = process.env.HOST
 const PORT = process.env.PORT || 3000
 const controller = require('./controller/controller')
-const path = require('path')
-const connection = app.use(controller).listen(3000)
-const io = socket(connection)
-console.log("Connectado:", HOST, ':', PORT)
+const io = socket(app.use(controller).listen(PORT))
 //OSC SERIAL PORT
 
-var SerialPort = require("serialport");
-
-
-//var virtualSerialPort = new SerialPort("message")
-
-virtualSerialPort.on("open", function () {
-    console.log("-- Connection opened --");
-   
-    virtualSerialPort.on("data", function (data) {
-        console.log("Data received: " + data);
-    })
-})
-
-
+var UDPPort = new osc.UDPPort({})
+UDPPort.on("message", function (oscMessage) { console.log(oscMessage) })
+UDPPort.open()
 
 var getIPAddresses = function () {
     var os = require("os"),
         interfaces = os.networkInterfaces(),
         ipAddresses = []
-    //   console.log("Interfaces",interfaces)
     for (var deviceName in interfaces) {
         var addresses = interfaces[deviceName]
-        //     console.log("Addresses on get IP",addresses)
+        console.log(addresses)
         for (var i = 0; i < addresses.length; i++) {
             var addressInfo = addresses[i]
-
-            if (addressInfo.family === "eth0" && !addressInfo.internal) {
+            if (addressInfo.family === "IPv4" && !addressInfo.internal) {
                 ipAddresses.push(addressInfo.address)
             }
-
         }
     }
     return ipAddresses
@@ -54,8 +37,9 @@ var getIPAddresses = function () {
 console.log("HOST", HOST)
 var udpPort = new osc.UDPPort({
     address: HOST,
-    localPort: 5000
+    localPort: PORT
 })
+
 udpPort.open()
 udpPort.on("ready", function () {
     io.sockets.setMaxListeners(1)
