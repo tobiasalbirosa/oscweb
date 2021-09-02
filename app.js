62e77dd844d99f3b8f6e35f557c496127a44362b
tobiasalbirosa <tobiasalbirosa@gmail.com>
2021-09-02 13:23:56 -0300

    m

--- app.js
+++ app.js
@@ -1,31 +1,57 @@
 'use strict'
 require('dotenv').config()
+var os = require("os")
 var socket = require('socket.io')
 const express = require('express')
 const controller = require('./controller/controller')
-
 //APP
 const app  = express()
 const HOST = process.env.HOST
-const PORT = process.env.PORT || 5000
+const TCPPORT = 5000
+const UDPPORT = 3000
+
+//MONTAMOS UN PUERTO TCP PARA LOS USUARIOS WEB
 app.engine('html', require('ejs').renderFile)
 app.set('view engine', 'html')
 app.use(controller)
-console.log(HOST,PORT)
-const server = app.listen(PORT)
+const server = app.listen(TCPPORT)
+//MONTAMOS UN PUERTO UDP PARA LOS MENSAJES OSC
+const osc = require('osc')
 const io = socket(server)
 
-var fp = require("find-free-port")
-for(let i = 0; i < 500; i++){
-  fp(i).then(([freep]) => {
-    console.log('found ' + freep)
-}).catch((err)=>{
-    console.error(err);
-});
+
+//getIPAddresses
+var getIPAddresses = function () {
+     let interfaces = os.networkInterfaces(),
+      ipAddresses = []
+  for (var deviceName in interfaces) {
+      var addresses = interfaces[deviceName]
+      console.log(addresses)
+      console.log(addresses.length)
+
+      for (var i = 0; i < addresses.length; i++) {
+          var addressInfo = addresses[i]
+          ipAddresses.push(addressInfo.address) 
+      }
+  }
 }
 
 
+getIPAddresses()
+
 //OSC SERIAL PORTvar 
+var UDPort = new osc.UDPPort({
+  localAddress: "oscweb.herokuapp.com",
+  localPort: UDPPORT,
+  metadata: true
+})
+console.log(HOST,UDPPORT)
+console.log(UDPort)
+UDPort.on("message", function (oscMessage) { console.log(oscMessage) })
+UDPort.on("error", function (error) { console.log(error) })
+UDPort.open()
+
+/*
 io.on("connect", function (socket) {
     console.log("connected id: ",socket.id)
 })
@@ -48,3 +74,4 @@ io.on('connection',function(socket) {
 
     })
   })
+  */
