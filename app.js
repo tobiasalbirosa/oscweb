c0ed00a98cc98c5cc298a845d742252dcc4aa7fe
tobiasalbirosa <tobiasalbirosa@gmail.com>
2021-09-02 00:18:07 -0300

    j

--- app.js
+++ app.js
@@ -6,31 +6,17 @@ const express = require('express')
 const controller = require('./controller/controller')
 var HOST = 'oscweb.herokuapp.com'
 var PORT = 443 
-
 //APP
 const app = express()
 app.engine('html', require('ejs').renderFile)
 app.set('view engine', 'html')
 app.use(controller)
-
 let socketApp = app.listen(PORT)
-console.log(" PORT: ", PORT)
 const io = socket(socketApp)
-//OSC UDP PORT
 var udpPort = new osc.UDPPort({
     address: HOST,
     localAddress: HOST,
-    localPort: 3000,
+    localPort: PORT,
     metadata: true
 })
-udpPort.open()
-udpPort.on("ready", function () {
-    io.sockets.setMaxListeners(1)
-    console.log("UDP Host:", HOST + ", Port:", udpPort.options.localPort)
-})
-udpPort.on("message", function (oscMessage) {
-    console.log(oscMessage)
-    io.emit('message', oscMessage)
-})
-udpPort.on("error", function (err) { console.log("ERROR ON PORT UDP: ", err) })
-console.log("udpPort: ", udpPort)
+//OSC UDP PORT
