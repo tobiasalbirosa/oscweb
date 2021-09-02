dea5ebef5859a9114b912cef9b83ee8112d483f4
tobiasalbirosa <tobiasalbirosa@gmail.com>
2021-09-02 16:52:58 -0300

    m

--- app.js
+++ app.js
@@ -6,10 +6,7 @@ const express = require('express')
 const controller = require('./controller/controller')
 //APP
 const app  = express()
-const HOST = process.env.HOST
 const TCPPORT =   process.env.PORT || 3000
-const UDPPORT = 5000
-
 //MONTAMOS UN PUERTO TCP PARA LOS USUARIOS WEB
 app.engine('html', require('ejs').renderFile)
 app.set('view engine', 'html')
@@ -19,7 +16,6 @@ console.log("THE PORT", TCPPORT)
 const osc = require('osc')
 const io = socket(app.listen(TCPPORT))
 //getIPAddresses
-
 io.on("message", function (message) {
   console.log("message: ",message)
 })
@@ -29,28 +25,3 @@ io.on("connect", function (socket) {
 io.on('error',function(error) {
   console.log('error', error);
 })
-//OSC SERIAL PORTvar 
-/*
-io.on("connect", function (socket) {
-    console.log("connected id: ",socket.id)
-})
-io.on('message',function(socket) {
-    console.log('message', socket.name);
-
-  })
-  io.on('error',function(error) {
-    console.log('error', error);
-
-  })
-io.on('connection',function(socket) {
-    console.log('made socket connection', socket.name);
-    socket.on('error', (error) => { 
-        console.log("error: ",error)
-
-    })
-    socket.on('message', (message) => { 
-        console.log("message: ",message)
-
-    })
-  })
-  */
