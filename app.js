6914b774e0bf409472357526b33c827c2c7019c5
tobiasalbirosa <tobiasalbirosa@gmail.com>
2021-09-02 01:04:04 -0300

    m

--- app.js
+++ app.js
@@ -1,52 +1,10 @@
 
 'use strict'
 require('dotenv').config()
-var socket = require('socket.io')
-var osc = require("osc")
 const express = require('express')
 const controller = require('./controller/controller')
-
 //APP
 const app = express()
 app.engine('html', require('ejs').renderFile)
 app.set('view engine', 'html')
-const HOST = process.env.HOST
 app.use(controller).listen(3000)
-
-/*
-socket.on('listening', () => {
-  let addr = socket.address();
-  console.log(`Listening for UDP packets at ${addr.address}:${addr.port}`);
-})
-socket.on('error', (err) => {
-  console.error(`UDP error: ${err.stack}`)
-})
-socket.on('message', (msg, rinfo) => {
-  console.log('Recieved UDP message')
-})
-socket.bind(8082)     // listen for UDP with dgra
-'use strict'
-require('dotenv').config()
-var socket = require('socket.io')
-var osc = require("osc")
-const express = require('express')
-const controller = require('./controller/controller')
-var HOST = 'oscweb.herokuapp.com'
-var PORT = 3000 
-//APP
-const app = express()
-app.engine('html', require('ejs').renderFile)
-app.set('view engine', 'html')
-app.use(controller)
-app.listen(3000)
-let socketApp = app.listen(serv)
-
-const io = socket(socketApp)
-var udpPort = new osc.UDPPort({
-    address: HOST,
-    localAddress: HOST,
-    localPort: PORT,
-    metadata: true
-})
-//OSC UDP PORT
-*/
