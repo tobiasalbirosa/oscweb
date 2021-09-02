95dedbb04b8c26a510aa9384f5590ec51abfd21c
tobiasalbirosa <tobiasalbirosa@gmail.com>
2021-09-02 01:13:35 -0300

    m

--- app.js
+++ app.js
@@ -3,12 +3,12 @@
 require('dotenv').config()
 var socket = require('socket.io')
 var osc = require("osc")
-const dgram = require('dgram');
-const UDPSocket = dgram.createSocket('udp4');
+const dgram = require('dgram')
 const express = require('express')
 const controller = require('./controller/controller')
 //APP
 const app = express()
 app.engine('html', require('ejs').renderFile)
 app.set('view engine', 'html')
-app.use(controller).listen(3000)
+app.use(controller)
+app.listen(3000)
