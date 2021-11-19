2b4952d8aa93a8fe76d03c110e4a8350fd20f8d4
tobjj <tobiasalbirosa@gmail.com>
2021-11-19 14:45:11 -0300

    viernes

--- public/scripts/sketch.js
+++ public/scripts/sketch.js
@@ -32,7 +32,7 @@ class Particle {
           fill(255,255,0,velocidad + 120)
     
         }
-        ellipse(this.particleX[i], this.particleY[i],(velocidad/100) + 0.1,(velocidad/100) + 0.1)
+        ellipse(this.particleX[i], this.particleY[i],(velocidad/100) + 0.5,(velocidad/100) + 0.5)
 
       }
 
